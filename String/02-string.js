4002253dd59648ace8757d5f703150baf2f14e60
andreso <usuario@MacBook-Pro-de-USUARIO.local>
2022-03-22 23:22:33 -0500

    Terminamos String

--- String/02-string.js
+++ String/02-string.js
@@ -30,3 +30,73 @@ console.log(productoNevera.concat('en descuento'));
 console.log("El producto " +  productoNevera + "tiene un valor de " + precio);
 
 console.log(`El producto ${producto} tiene un valor de ${precio}` );
+
+
+//Eliminar espacios en blanco
+const producto5 = '     Monitor de 20 pulgadas              ';
+console.log(producto5);
+console.log(producto5.length);
+
+//Eliminar espacio al inicio
+console.log(producto5.trimStart());
+
+
+//Eliminar espacio al final
+console.log(producto5.trimEnd());
+
+
+//Eliminar ambas direccion espacios
+console.log(producto5.trimStart().trimEnd());
+
+
+//Eliminar ambas direccion espacios
+console.log(producto5.trim());
+
+const producto6 ='Monitor de 20 pulgadas';
+
+//Reemplazar texto
+console.log(producto6.replace('pulgadas','"'));
+console.log(producto6.replace('Monitor', 'Monitor Curvo'));
+
+//Extraer o cortar texto
+console.log(producto6.slice(0, 10));
+console.log(producto6.slice(8));
+console.log(producto6.slice(2, 1));
+
+
+//Alternativa a Slice
+console.log(producto6.substring(0, 10));
+console.log(producto6.substring(7, 1));
+
+//Circulo para Primera Letra
+const usuario = "Juan";
+console.log(usuario.substring(0,1));
+console.log(usuario.charAt(0));
+
+const producto7 ='Monitor de 20 pulgadas';
+
+//Repetir una cadena de texto
+const texto = ' en Promocion'.repeat(3);
+console.log(texto);
+console.log(`${producto} ${texto} !!!!!`);
+
+//Dividir texto
+const actividad= "Estoy aprendiendo JavaScript Moderno";
+console.log(actividad.split(" "));
+
+const hobbies = 'Leer , Musica, Escribir, Aprender, Llorar';
+console.log(hobbies.split(" ,"));
+
+const producto8= "Monitor de 20 pulgadas";
+
+//Minusculas
+console.log(producto8.toLocaleLowerCase());
+
+//Mayusculas
+console.log(producto8.toLocaleUpperCase());
+
+//Pasar numero a String
+
+const precio_1 = 300;
+console.log(precio_1);
+console.log(precio_1.toString());
