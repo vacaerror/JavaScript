c34efa11e161406e2136ce2c666ca6402bc7d119
andreso <usuario@MacBook-Pro-de-USUARIO.local>
2022-03-24 15:38:26 -0500

    Agregar Numeros

---
+++ Numeros/01-numero.js
@@ -0,0 +1,18 @@
+const numero1=20;
+const numero2=60;
+
+
+console.log(numero1+numero2);
+
+const numero3=2.4;
+
+console.log(numero3);
+
+const numero4= 0.10;
+const numero5= -19;
+
+console.log(numero4);
+console.log(numero5);
+
+const numero6 = new Number(80);
+console.log(numero6);
